ddeee67358a169b57f76d178c6c73796ea9645af
VISHAL-VADODARIYA <vadodariyavishal2@gmail.com>
2023-08-21 18:58:21 +0530

    change

--- practice/MongoDB/index.js
+++ practice/MongoDB/index.js
@@ -6,26 +6,61 @@ mongoose
   .catch((err) => console.error("Could not connect to MongoDB...", err));
 
 const courseSchema = mongoose.Schema({
-  name: { type: String, required: true },
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    // match: /pattern/,
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: ["web", "mobile", "network"],
+    lowercase: true,
+    // uppercase: true,
+    trim: true,
+  },
   author: String,
-  tags: [String],
+  tags: {
+    type: Array,
+    validate: {
+      isAsync: true,
+      validator: function (v, callback) {
+        setTimeout(() => {
+          const result = v && v.lenght > 0;
+          callback(result);
+        }, 2000);
+      },
+      message: "A Course should have at least one tag",
+    },
+  },
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
+  price: Number,
 });
 
 const Course = mongoose.model("Course", courseSchema);
 async function createCourse() {
   const course = new Course({
-    // name: "ReactJS Course",
+    name: "AngularJS Course",
+    category: "web",
     author: "vishal",
-    tags: ["React", "frontend"],
+    tags: ["Frontend"],
     isPublished: true,
+    price: 15,
   });
-  try{
+  try {
+    // course.validate((err) => {
+    //   if (err) {
+    //   }
+    // });
     const result = await course.save();
     console.log(result);
+  } catch (ex) {
+    // console.log(ex.message);
+    for (field in ex.errors) console.log(ex.errors[field].message);
   }
-  catch(ex){console.log(ex.message)}
 }
 createCourse();
 
@@ -91,9 +126,12 @@ async function updateCourse() {
 async function removeCourse(id) {
   const result = await Course.deleteOne({ _id: id });
   console.log(result);
+
+  //we can delete from any value like here we can use name for delete all the data which have same name
   const answer = await Course.deleteMany({ _id: id });
   console.log(answer);
+
   const course = await Course.findByIdAndRemove(id);
   console.log(course);
 }
-// removeCourse("helloID Here");
+// removeCourse("helloID");
